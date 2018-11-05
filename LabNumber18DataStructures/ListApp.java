ab31e5f528d23b93844f9b2c69bd0da28e905b8a
bryan <bryan_hallman29@yahoo.com>
2018-11-05 08:26:18 -0500

    Lab 18 Initial Commit

---
+++ LabNumber18DataStructures/ListApp.java
@@ -0,0 +1,41 @@
+package LabNumber18DataStructures;
+public class ListApp {
+
+	public static void main(String[] args) {
+		testArrayList();
+		testLinkedList();
+	}
+	
+	private static void testArrayList() {
+		MyList list = new MyArrayList();
+		
+		list.addAtBeginning("B");
+		list.addAtBeginning("A");
+		list.addAtEnd("C");
+		list.addAtEnd("D");
+		list.removeFromBeginning();
+		list.removeFromEnd();
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+	}
+	
+	private static void testLinkedList() {
+		MyList list = new MyLinkedList();
+		
+		list.addAtBeginning("B");
+		list.addAtBeginning("A");
+		list.addAtEnd("C");
+		list.addAtEnd("D");
+		list.removeFromBeginning();
+		list.removeFromEnd();
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+	}
+
+}
---
+++ LabNumber18DataStructures/MyLinkedList.java
@@ -0,0 +1,110 @@
+package LabNumber18DataStructures;
+public class MyLinkedList implements MyList {
+
+	// head is the first node in the list. if null, the list is empty.
+	private Node head = null;
+	// for convenience, keep track of how many items are in the list
+	private int length = 0;
+	
+	@Override
+	public void addAtBeginning(String data) {
+		// add a new node and set it as the new head
+		Node newNode = new Node(data);
+		newNode.setNext(head);
+		head = newNode;
+		length++;
+	}
+	
+	@Override
+	public void removeFromBeginning() {
+		// find the second node and set it as the new head
+		head = head.getNext();
+		length--;
+	}
+	
+	@Override
+	public void addAtEnd(String data) {
+		// TODO You do this one.
+	}
+	
+	@Override
+	public void removeFromEnd() {
+		if (length < 2) {
+			// The beginning is the end.
+			removeFromBeginning();
+		} else {
+			// find the node right before the end.
+			Node beforeTail = getNodeAt(length - 2);
+			// set its next to null. This ends the list here.
+			beforeTail.setNext(null);
+			length--;
+		}
+	}
+	
+	@Override
+	public boolean removeAt(intindex) {
+		
+	}
+	
+	@Override
+	public String get(int index) {
+		return getNodeAt(index).getData();
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		// start at the head
+		Node node = head;
+		// follow the links between nodes until it reaches the end 
+		while (node != null) {
+			sb.append(node.toString());
+			node = node.getNext();
+		}
+		return sb.toString();
+	}
+	
+	@Override
+	public int size() {
+		return length;
+	}
+	
+	private Node getNodeAt(int index) {
+		// start at the head
+		Node node = head;
+		// follow the links between nodes until it counts off the right number
+		for (int i = 0; i < index; i++) {
+			if (node == null) {
+				// In case we run out of nodes before we get up to the desired index, return null
+				return null;
+			}
+			node = node.getNext();
+		}
+		return node;
+	}
+	
+	@Override
+	public boolean removeAt(intindex) {
+		for (int i = 0; i < index; i++) {
+			if (node == null) {
+				
+				return null;
+
+				Node atIndex = getNodeAt(I);
+				length--;
+			}
+	}
+	}
+
+@Override
+	public boolean addAt(intindex) {
+		for (int i = 0; i > index; i--) {
+			if (node == null) {
+				
+				return index;
+
+				Node atIndex = getNodeAt(I);
+				length++;
+			}
+	}
+
+}
